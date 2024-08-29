c15cfed3c8486d278ad41b7790015198022d25fe
git_learning <nazarzybin@gmail.com>
2024-08-29 13:21:20 +0300

    home1 update

--- home_work_5_1.py
+++ home_work_5_1.py
@@ -1,22 +1,22 @@
-my_list = input('Ведіть рядок: ')
+my_string = input('Ведіть рядок: ')
 
 import string
 import keyword
 
 is_valid = True
 
-if my_list[0].isdigit() == True:
+if my_string[0].isdigit():
     is_valid = False
 
-for i in my_list:
-    if i.isupper() == True:
+for i in my_string:
+    if i.isupper():
         is_valid = False
         break
 
-for i in my_list:
+for i in my_string:
     if i in string.punctuation:
         if i == '_':
-            if my_list.count('_') > 1:
+            if my_string.count('_') > 1:
                 is_valid = False
                 break
         else:
@@ -26,7 +26,7 @@ for i in my_list:
         break
 
 kwlist = keyword.kwlist
-if my_list in kwlist:
+if my_string in kwlist:
     is_valid = False
 
 print(is_valid)
